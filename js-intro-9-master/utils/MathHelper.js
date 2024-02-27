d6bec1aae2ef9283c16a95d9a7a1d725fdf49bac
ralsilwadi <ralsilwadi@gmail.com>
2024-02-26 18:10:29 -0600

    ...

---
+++ js-intro-9-master/utils/MathHelper.js
@@ -0,0 +1,9 @@
+function generateRandomNumber(a, b) {
+    let max = Math.max(a, b);
+    let min = Math.min(a, b);
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Exporting your reusable methods
+module.exports.generateRandomNumber = generateRandomNumber;
