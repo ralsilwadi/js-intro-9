fe93f23d05489732fec1ae7c47b895b4cc65453b
ralsilwadi <ralsilwadi@gmail.com>
2024-03-02 11:02:13 -0600

    ...

--- class.js
+++ class.js
@@ -1,10 +1,8 @@
 const {generateRandomNumber} = require('/Users/ralsilwadi/Desktop/js-intro-9-rabe/js-intro-9-master/utils/MathHelper.js')
 
-let r1 = generateRandomNumber(1,10)
-let r2 = generateRandomNumber(1,10)
-smallNum = (Math.min(r1, r2))
-bigNum = (Math.max(r1, r2))
+i = 0;
 
-for (let i = smallNum; i <= bigNum; i++) {
-    console.log(i)
+while (i <= 25) {
+    console.log(i*2)
+    i++
 }
