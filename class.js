2a169a42502592c19055f47002ffbf9295fb6ce0
ralsilwadi <ralsilwadi@gmail.com>
2024-03-04 19:39:49 -0600

    ...

--- class.js
+++ class.js
@@ -1,9 +1,8 @@
 const {generateRandomNumber} = require('/Users/ralsilwadi/Desktop/js-intro-9-rabe/js-intro-9-master/utils/MathHelper.js')
 
-for (let i = 0; i <= 23; i++) {
-    for (let j = 0; j <= 59; j++) {
-        for (let k = 0; k <= 59; k++) {
-            console.log(i + ':' + j + ':' + k)
-        }
-    }
+const numbers = [3, 4, 7, 3, 2, 2, 7];
+let product = 1
+for (i = 0; i < numbers.length; i++) {
+    if (i % 2 !== 0) product *= numbers[i]
 }
+console.log(product)
