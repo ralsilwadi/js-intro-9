4f06337bfbb8d16c7e7399a0ea55841da6d07064
ralsilwadi <ralsilwadi@gmail.com>
2024-03-11 15:11:55 -0500

    ...

--- class.js
+++ class.js
@@ -1,3 +1,18 @@
-const names = ['John', 'Jane', 'Alex', 'Max'];
+const cities = ['Rome', 'Kyiv', 'Berlin', 'Brugge', 'LA'];
 
-names.forEach((name) => name[0].toLowerCase() === 'j' && console.log(name));
+const fourCity = cities.find(city => city.length === 4);
+
+const lastFourCity = cities.slice().reverse().find(city => city.length === 4);
+
+const sixCity = cities.slice().reverse().find(city => city.length === 6);
+
+const indexOfLA = cities.indexOf('LA');
+
+const firstCityWithI = cities.find(city => city.toLowerCase().includes('i'));
+
+console.log(fourCity);
+console.log(lastFourCity);
+console.log(sixCity);
+console.log(indexOfLA);
+console.log(firstCityWithI);
+
